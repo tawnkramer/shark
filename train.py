4fe75b41d2c354f1ec9a3d83749d493af3d8f939
Tawn Kramer <tawnkramer@gmail.com>
2017-08-14 17:38:12 -0700

    fix typo

--- train.py
+++ train.py
@@ -142,7 +142,7 @@ def generator(samples, batch_size=32, perc_to_augment=0.5, transposeImages=False
                         image = np.fliplr(image)
                         images.append(image)
 
-                        if conf.model_output_dim == 2:
+                    if conf.model_output_dim == 2:
                         controls.append([-steering, throttle])
                     else:
                         controls.append([-steering])
